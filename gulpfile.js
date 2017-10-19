0fd16ccd9795823d4466002819470cef45a08bdd
Jeremy Voros <jeremyvoros@gmail.com>
2017-10-19 09:36:47 -0600

    gulp build system

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const gulp = require('gulp');
+
+const del = require('del');
+const handlebars = require('gulp-compile-handlebars');
+const rename = require('gulp-rename');
+const stylus = require('gulp-stylus');
+
+const config = {
+  src: "./src",
+  pages: "./src/pages/*.hbs",
+  partials: "./src/partials",
+  styl: "./src/styl/*.styl",
+  assets: "./src/assets/**/*",
+  cname: "./src/CNAME",
+  dest: "./docs",
+  html: "./docs/*.html",
+  css: "./docs/css",
+  destassets: "./docs/assets/**/*"
+}
+
+// CLEANERS
+gulp.task('cleanall', () => del(config.dest));
+gulp.task('cleanhtml', () => del(config.html));
+gulp.task('cleancss', () => del(config.css));
+gulp.task('cleanassets', () => del(config.destassets));
+
+// BUILDERS
+gulp.task('stylus', () => {
+  return gulp.src(config.styl)
+    .pipe(stylus())
+    .pipe(gulp.dest(config.css));
+});
+
+gulp.task('html', () => {
+  return gulp.src(config.pages)
+    .pipe(handlebars({}, {
+      ignorePartials: true,
+      batch: [config.partials]
+    }))
+    .pipe(rename({
+      extname: '.html'
+    }))
+    .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('assets', () => {
+  return gulp.src(config.assets, { base:'./src' })
+  .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('cname', () => {
+  return gulp.src(config.cname)
+  .pipe(gulp.dest(config.dest));
+});
+
+// WATCHERS
+gulp.task('watchers', () => {
+  gulp.watch(config.pages, gulp.series('cleanhtml', 'html'));
+  gulp.watch(config.partials, gulp.series('cleanhtml', 'html'));
+  gulp.watch(config.styl, gulp.series('cleancss', 'stylus'));
+  gulp.watch(config.assets, gulp.series('cleanassets', 'assets'));
+});
+
+// RUNNERS
+const buildList = [
+  'cleanall',
+  'cname',
+  'html',
+  'stylus',
+  'assets'
+];
+
+gulp.task('build', gulp.series(...buildList));
+gulp.task('watch', gulp.series(...buildList, 'watchers'));
